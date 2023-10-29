a7979c0e0a6cde7937c6a60585c492c449f4922f
Daniel Neel <dneel10@gmail.com>
2023-10-29 10:02:16 -0400

    trying to get stimulus working

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="passenger"
 export default class extends Controller {
   connect() {
-    console.log("Hello rails", this.element)
+    console.log("Hello rails")
   }
 }
