1b97602be8a04ef36d79b71b09e343b5bd3ff687
Daniel Neel <dneel10@gmail.com>
2023-10-30 13:41:46 -0400

    add stimulus controllers to add additional passengers to the booking

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,7 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
+  static targets = [ "passengerFields", "passengerOutput" ]
+  static values = { "passengerCount": Number }
+
   connect() {
-    console.log("Hello rails")
+    console.log("Hello from passengers")
+  }
+
+  add() {
+    this.passengerCountValue++
+    const temp = this.passengerFieldsTarget
+    const clone = temp.content.cloneNode(true)
+
+    this.passengerOutputTarget.appendChild(clone)
+  }
+
+  remove() {
+    this.passengerCountValue--
   }
 }
